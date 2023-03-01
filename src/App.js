269978511201103402f860191744dc57f4000662
ktkim <kangtae49@gmail.com>
2023-03-01 08:39:51 +0000

    success deploy

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 import { hot } from 'react-hot-loader/root';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-// import { QueryClient, QueryClientProvider } from "react-query";
-// import { ReactQueryDevtools } from "react-query/devtools";
+import { HashRouter,  Routes, Route } from 'react-router-dom';
 
 import Main from './pages/Main';
 import ExchangeRate from './pages/ExchangeRate'
 
 
 function App() {
+  
   return (
-      <BrowserRouter basename={process.env.PUBLIC_URL}>
-        <Routes>
-          <Route exact path="/" element={<Main />}></Route>
-          <Route path="exchange-rate" element={<ExchangeRate />}></Route>
-        </Routes>
-      </BrowserRouter>
+      <HashRouter >
+        <div>
+          <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/exchange-rate" element={<ExchangeRate />} />
+          </Routes>
+        </div>
+      </HashRouter>
   );
 }
 
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,7 +1,12 @@
+import { Link } from 'react-router-dom';
 
 
 function Main() {
-    return (<div>Main</div>);
+    return (
+    <div>
+        <Link to={'/exchange-rate'}>Exchange Rate</Link>
+    </div>
+    );
 }
 
 export default Main;
